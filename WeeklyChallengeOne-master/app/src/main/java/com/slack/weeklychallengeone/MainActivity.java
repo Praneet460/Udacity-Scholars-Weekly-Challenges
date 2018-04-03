824b60c63b005c0654904141faf786a9d3add20c
Rajat Kumar Gupta <rajatcube@gmail.com>
2018-04-03 19:57:38 +0530

    Added my name in the list

--- WeeklyChallengeOne-master/app/src/main/java/com/slack/weeklychallengeone/MainActivity.java
+++ WeeklyChallengeOne-master/app/src/main/java/com/slack/weeklychallengeone/MainActivity.java
@@ -38,14 +38,13 @@ public class MainActivity extends AppCompatActivity {
 
         mArrayList = new ArrayList<>();
 
+        mArrayList.add(new Model("Rajat Kumar Gupta"));
         mArrayList.add(new Model("Constraint Layout"));
         mArrayList.add(new Model("Linear Layout"));
         mArrayList.add(new Model("Relative Layout"));
         mArrayList.add(new Model("Card View"));
         mArrayList.add(new Model("Scroll Views"));
         mArrayList.add(new Model("Grid View"));
-
-
 
         mAdapter = new MyAdapter(mArrayList);
         mRecyclerView.setAdapter(mAdapter);
