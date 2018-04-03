5350415e865d1321ca4365ac79f7d346fcb8208b
Kaushal <kaushal.dhruw@talentica.com>
2018-04-03 19:54:50 +0530

    Bugs fixed and removed unnecessary tracked files and folders

--- WeeklyChallengeOne-master/app/src/main/java/com/slack/weeklychallengeone/MainActivity.java
+++ WeeklyChallengeOne-master/app/src/main/java/com/slack/weeklychallengeone/MainActivity.java
@@ -14,13 +14,13 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Model> mArrayList;
     private MyAdapter mAdapter;
+    private RecyclerView mRecyclerView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        set ContentView(R.layout.ativity_main);
-
+        setContentView(R.layout.activity_main);
         initViews();
         loadData();
 
@@ -30,8 +30,8 @@ public class MainActivity extends AppCompatActivity {
     private void initViews(){
         mRecyclerView = findViewById(R.id.recyler_view);
         mRecyclerView.setHasFixedSize(true);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(thi);
-        mRecyclerViews.setLayoutManager(layoutManager);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
     }
 
 
@@ -39,23 +39,18 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-/*    private void loadData(){
+    private void loadData(){
 
         mArrayList = new ArrayList<>();
-
         mArrayList.add(new Model("Constraint Layout"));
         mArrayList.add(new Model("Linear Layout"));
         mArrayList.add(new Model("Relative Layout"));
         mArrayList.add(new Model("Card View"));
         mArrayList.add(new Model("Scroll Views"));
         mArrayList.add(new Model("Grid View"));
-
-
-
         mAdapter = new MyAdapter(mArrayList);
-        //mRecyclerView.setAdapter(mAdapter);
-
-    }*/
+        mRecyclerView.setAdapter(mAdapter);
+    }
 
 
 
