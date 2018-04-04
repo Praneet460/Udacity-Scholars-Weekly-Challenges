f096fc4ef77cb6a86619fe49a0efe7c865976225
Suneet Srivastava <suneetbond91@gmail.com>
2018-04-05 03:08:24 +0530

    Challenge1 Done

--- WeeklyChallengeOne-master/app/src/main/java/com/slack/weeklychallengeone/Utils/Model.java
+++ WeeklyChallengeOne-master/app/src/main/java/com/slack/weeklychallengeone/Utils/Model.java
@@ -1,5 +1,11 @@
 package com.slack.weeklychallengeone.Utils;
 
+/*
+    The model of the recycler view items
+    @author : codedsun
+    @date : 05/April/2018
+    @param : name - for the name of th
+ */
 public class Model {
     private String name;
 
