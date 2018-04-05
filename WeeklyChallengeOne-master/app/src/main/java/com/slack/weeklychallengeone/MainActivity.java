57c05ad8fcb5f4a8fc968c79c1e9790b9a02531f
Ashish Kumar <ashishkumar160@gmail.com>
2018-04-05 21:51:11 +0530

    Fixed the issues

--- WeeklyChallengeOne-master/app/src/main/java/com/slack/weeklychallengeone/MainActivity.java
+++ WeeklyChallengeOne-master/app/src/main/java/com/slack/weeklychallengeone/MainActivity.java
@@ -14,35 +14,25 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Model> mArrayList;
     private MyAdapter mAdapter;
-
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        set ContentView(R.layout.ativity_main);
-
+        setContentView(R.layout.activity_main);
         initViews();
         loadData();
-
     }
-
 
     private void initViews(){
         mRecyclerView = findViewById(R.id.recyler_view);
         mRecyclerView.setHasFixedSize(true);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(thi);
-        mRecyclerViews.setLayoutManager(layoutManager);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
     }
 
-
-
-
-
-
-/*    private void loadData(){
-
+    private void loadData(){
         mArrayList = new ArrayList<>();
-
         mArrayList.add(new Model("Constraint Layout"));
         mArrayList.add(new Model("Linear Layout"));
         mArrayList.add(new Model("Relative Layout"));
@@ -50,13 +40,7 @@ public class MainActivity extends AppCompatActivity {
         mArrayList.add(new Model("Scroll Views"));
         mArrayList.add(new Model("Grid View"));
 
-
-
         mAdapter = new MyAdapter(mArrayList);
-        //mRecyclerView.setAdapter(mAdapter);
-
-    }*/
-
-
-
+        mRecyclerView.setAdapter(mAdapter);
+    }
 }
