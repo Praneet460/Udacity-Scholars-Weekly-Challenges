be2811bc9598742ae2fd0d8bba2e3259364e12ec
YuganshT79 <yugansh39@gmail.com>
2018-04-06 21:56:37 +0530

    Fixed errors and Redesigned app - @YuganshT79

--- WeeklyChallengeOne-master/app/src/main/java/com/slack/weeklychallengeone/MainActivity.java
+++ WeeklyChallengeOne-master/app/src/main/java/com/slack/weeklychallengeone/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 
 import com.slack.weeklychallengeone.Utils.Model;
 import com.slack.weeklychallengeone.Utils.MyAdapter;
@@ -13,50 +14,40 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Model> mArrayList;
-    private MyAdapter mAdapter;
+    MyAdapter mAdapter;
+    RecyclerView mRecyclerView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        set ContentView(R.layout.ativity_main);
+        setContentView(R.layout.activity_main);
 
-        initViews();
         loadData();
+        initViews();
 
     }
 
 
-    private void initViews(){
-        mRecyclerView = findViewById(R.id.recyler_view);
+    private void initViews() {
+        mRecyclerView = findViewById(R.id.recycler_view);
         mRecyclerView.setHasFixedSize(true);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(thi);
-        mRecyclerViews.setLayoutManager(layoutManager);
+        mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2,1));
+        mAdapter = new MyAdapter(mArrayList);
+        mRecyclerView.setAdapter(mAdapter);
     }
 
 
-
-
-
-
-/*    private void loadData(){
+    private void loadData() {
 
         mArrayList = new ArrayList<>();
 
-        mArrayList.add(new Model("Constraint Layout"));
-        mArrayList.add(new Model("Linear Layout"));
-        mArrayList.add(new Model("Relative Layout"));
-        mArrayList.add(new Model("Card View"));
-        mArrayList.add(new Model("Scroll Views"));
-        mArrayList.add(new Model("Grid View"));
-
-
-
-        mAdapter = new MyAdapter(mArrayList);
-        //mRecyclerView.setAdapter(mAdapter);
-
-    }*/
-
-
+        mArrayList.add(new Model("Constraint Layout", getResources().getString(R.string.cons_layout_desc)));
+        mArrayList.add(new Model("Linear Layout", getResources().getString(R.string.linear_layout_desc)));
+        mArrayList.add(new Model("Relative Layout", getResources().getString(R.string.rel_layout_Desc)));
+        mArrayList.add(new Model("Card View", getResources().getString(R.string.cardview_desc)));
+        mArrayList.add(new Model("Scroll Views", getResources().getString(R.string.scrollview_desc)));
+        mArrayList.add(new Model("Grid View", getResources().getString(R.string.gridview_desc)));
+    }
 
 }
